2bf7622bbba88f6e7a3026a263c9561382a4ac6a
kazumi hirata <hiratakazumi2020@gmail.com>
2021-05-07 01:10:45 +0900

    add crud

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -2,12 +2,36 @@
 
 namespace App\Model\Table;
 
+use Cake\Validation\Validator;
+use Cake\Event\EventInterface;
 use Cake\ORM\Table;
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config): void
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave(EventInterface  $event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->notEmptyString('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmptyString('body')
+            ->minLength('body', 10);
+
+        return $validator;
     }
 }
